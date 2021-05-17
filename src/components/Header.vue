f42b80f77111a39d835b2b6e893c2a0972a866b0
Miquel Castro <castro-miquel@hotmail.com>
2021-05-17 22:14:35 +0200

    Header of app

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <h1>Image dominating color</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped>
+header {
+  width: 100%;
+  background-color: #42b983;
+  color: white;
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
+h1 {
+  text-align: center;
+}
+</style>
