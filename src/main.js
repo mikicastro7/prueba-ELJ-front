56d521ecc7b9b03151f956e9d7a47089af534919
Miquel Castro <castro-miquel@hotmail.com>
2021-05-18 10:12:35 +0200

    Adding font awsome package upload fa

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
-import { createApp } from 'vue'
+import {
+    createApp
+} from 'vue'
 import App from './App.vue'
+import {
+    library
+} from "@fortawesome/fontawesome-svg-core";
+import {
+    faUpload
+} from "@fortawesome/free-solid-svg-icons";
+import {
+    FontAwesomeIcon
+} from "@fortawesome/vue-fontawesome";
 
-createApp(App).mount('#app')
+library.add(faUpload);
+
+
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount('#app')
